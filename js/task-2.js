60c1e5a7009b3ddd9fc72e0e395afaadb07936bc
OlgaKozoriz07 <161890591+OlgaKozoriz07@users.noreply.github.com>
2024-07-12 23:36:34 +0200

    markup

--- js/task-2.js
+++ js/task-2.js
@@ -18,7 +18,7 @@ class Storage {
         this.items.push(newItem);
     }
     removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
+    this.#items = this.items.filter(item => item !== itemToRemove);
     }
 }
 
